55ec2ed49f38588881b8226f349c9bbbe9abd4b5
Ahmed Hadziomerovic <91010342+Ahmed-hadzi@users.noreply.github.com>
2025-02-03 17:32:21 +0100

    Update ArduinoOSD.ino

--- ArduinoOSD.ino
+++ ArduinoOSD.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>
 
-#define VISTA_TX 10  // Arduino TX to Vista RX
-#define VISTA_RX 11  // Arduino RX to Vista TX
+#define OSD_TX 7  // Arduino TX to Vista RX
+#define OSD_RX 8  // Arduino RX to Vista TX
 
-SoftwareSerial vistaSerial(VISTA_RX, VISTA_TX);
+SoftwareSerial vistaSerial(OSD_TX, OSD_RX);
 
 void sendMSP(uint8_t cmd, const uint8_t *data, uint8_t dataSize) {
     uint8_t checksum = 0;
@@ -30,6 +30,15 @@ void sendBatteryData(float voltage, uint8_t batteryPercentage) {
     sendMSP(109, analogData, sizeof(analogData));  // 109 = MSP_ANALOG
 }
 
+/*void sendArmingStatus(bool armed) {
+    uint8_t statusData[8] = {0};  // Initialize all bytes to 0
+
+    statusData[2] = armed ? 1 : 0;  // Arming flag (1 = armed, 0 = disarmed)
+    statusData[3] = 0;  // High byte of flight mode flags (not used)
+    
+    sendMSP(101, statusData, sizeof(statusData));  // MSP_STATUS message
+}*/
+
 void setup() {
     vistaSerial.begin(115200);  // MSP protocol baud rate
 }
@@ -38,7 +47,9 @@ void loop() {
     float batteryVoltage = 3.7 * 4;  // Example for 4S LiPo battery (14.8V)
     uint8_t batteryPercentage = (batteryVoltage - 14.0) * 100 / (16.8 - 14.0);
 
+    //bool armed = false;
     sendBatteryData(batteryVoltage, batteryPercentage);
+    //sendArmingStatus(armed);
 
     delay(1000);  // Send data every second
 }
