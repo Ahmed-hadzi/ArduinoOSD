fe6a9c404f7319a9cf398bc79ecbb4195f6d15ac
Ahmed Hadziomerovic <91010342+Ahmed-hadzi@users.noreply.github.com>
2025-03-05 22:59:27 +0100

    OSD working

---
+++ djihdfpv_mavlink_to_msp_V2/djihdfpv_mavlink_to_msp_V2.ino
@@ -0,0 +1,145 @@
+#include <MSP.h>
+#include "MSP_OSD.h"
+#include "flt_modes.h"
+#include "OSD_positions_config.h"
+
+#define mspSerial Serial4
+
+MSP msp;
+
+uint32_t previousMillis_MSP = 0;
+const uint32_t next_interval_MSP = 100;
+
+uint8_t vbat = 85;
+uint16_t rssi = 0;
+uint8_t battery_remaining = 48;
+char craftname[15] = "AHMED";
+int16_t amperage = 23;
+uint16_t mAhDrawn = 40;
+float f_mAhDrawn = 0.0;
+uint8_t batteryState = 0;       // voltage color 0==white, 1==red
+uint16_t batteryVoltage = 8.4;
+uint32_t general_counter = 0;
+
+void setup()
+{
+    mspSerial.begin(115200);
+    msp.begin(mspSerial);
+}
+
+void loop()
+{
+    uint32_t currentMillis_MSP = millis();
+    if ((uint32_t)(currentMillis_MSP - previousMillis_MSP) >= next_interval_MSP) {
+        previousMillis_MSP = currentMillis_MSP;
+        send_msp_to_airunit();
+        general_counter += next_interval_MSP;
+    }
+}    
+
+void show_text(char (*text)[15])
+{
+    memcpy(craftname, *text, sizeof(craftname));
+}
+
+msp_battery_state_t battery_state = {0};
+msp_name_t name = {0};
+msp_status_BF_t status_BF = {0};
+msp_analog_t analog = {0};
+
+void send_msp_to_airunit()
+{
+    memcpy(name.craft_name, craftname, sizeof(craftname));
+    msp.send(MSP_NAME, &name, sizeof(name));
+    
+    analog.vbat = 84;
+    analog.rssi = 2000;
+    analog.amperage = 8500;
+    msp.send(MSP_ANALOG, &analog, sizeof(analog));
+
+    battery_state.mAhDrawn = 999;
+    msp.send(MSP_BATTERY_STATE, &battery_state, sizeof(battery_state));
+
+    send_osd_config();
+}
+
+msp_osd_config_t msp_osd_config = {0};
+
+void send_osd_config()
+{
+
+    msp_osd_config.osd_item_count = 56;
+    msp_osd_config.osd_stat_count = 24;
+    msp_osd_config.osd_timer_count = 2;
+    msp_osd_config.osd_warning_count = 16;              // 16
+    msp_osd_config.osd_profile_count = 1;              // 1
+    msp_osd_config.osdprofileindex = 1;                // 1
+    msp_osd_config.overlay_radio_mode = 0;             //  0
+
+    msp_osd_config.osd_rssi_value_pos = osd_rssi_value_pos;
+    msp_osd_config.osd_main_batt_voltage_pos = osd_main_batt_voltage_pos;
+    msp_osd_config.osd_crosshairs_pos = osd_crosshairs_pos;
+    msp_osd_config.osd_artificial_horizon_pos = osd_artificial_horizon_pos;
+    msp_osd_config.osd_horizon_sidebars_pos = osd_horizon_sidebars_pos;
+    msp_osd_config.osd_item_timer_1_pos = osd_item_timer_1_pos;
+    msp_osd_config.osd_item_timer_2_pos = osd_item_timer_2_pos;
+    msp_osd_config.osd_flymode_pos = osd_flymode_pos;
+    msp_osd_config.osd_craft_name_pos = osd_craft_name_pos;
+    msp_osd_config.osd_throttle_pos_pos = osd_throttle_pos_pos;
+    msp_osd_config.osd_vtx_channel_pos = osd_vtx_channel_pos;
+    msp_osd_config.osd_current_draw_pos = osd_current_draw_pos;
+    msp_osd_config.osd_mah_drawn_pos = osd_mah_drawn_pos;
+    msp_osd_config.osd_gps_speed_pos = osd_gps_speed_pos;
+    msp_osd_config.osd_gps_sats_pos = osd_gps_sats_pos;
+    msp_osd_config.osd_altitude_pos = osd_altitude_pos;
+    msp_osd_config.osd_roll_pids_pos = osd_roll_pids_pos;
+    msp_osd_config.osd_pitch_pids_pos = osd_pitch_pids_pos;
+    msp_osd_config.osd_yaw_pids_pos = osd_yaw_pids_pos;
+    msp_osd_config.osd_power_pos = osd_power_pos;
+    msp_osd_config.osd_pidrate_profile_pos = osd_pidrate_profile_pos;
+    msp_osd_config.osd_warnings_pos = osd_warnings_pos;
+    msp_osd_config.osd_avg_cell_voltage_pos = osd_avg_cell_voltage_pos;
+    msp_osd_config.osd_gps_lon_pos = osd_gps_lon_pos;
+    msp_osd_config.osd_gps_lat_pos = osd_gps_lat_pos;
+    msp_osd_config.osd_debug_pos = osd_debug_pos;
+    msp_osd_config.osd_pitch_angle_pos = osd_pitch_angle_pos;
+    msp_osd_config.osd_roll_angle_pos = osd_roll_angle_pos;
+    msp_osd_config.osd_main_batt_usage_pos = osd_main_batt_usage_pos;
+    msp_osd_config.osd_disarmed_pos = osd_disarmed_pos;
+    msp_osd_config.osd_home_dir_pos = osd_home_dir_pos;
+    msp_osd_config.osd_home_dist_pos = osd_home_dist_pos;
+    msp_osd_config.osd_numerical_heading_pos = osd_numerical_heading_pos;
+    msp_osd_config.osd_numerical_vario_pos = osd_numerical_vario_pos;
+    msp_osd_config.osd_compass_bar_pos = osd_compass_bar_pos;
+    msp_osd_config.osd_esc_tmp_pos = osd_esc_tmp_pos;
+    msp_osd_config.osd_esc_rpm_pos = osd_esc_rpm_pos;
+    msp_osd_config.osd_remaining_time_estimate_pos = osd_remaining_time_estimate_pos;
+    msp_osd_config.osd_rtc_datetime_pos = osd_rtc_datetime_pos;
+    msp_osd_config.osd_adjustment_range_pos = osd_adjustment_range_pos;
+    msp_osd_config.osd_core_temperature_pos = osd_core_temperature_pos;
+    msp_osd_config.osd_anti_gravity_pos = osd_anti_gravity_pos;
+    msp_osd_config.osd_g_force_pos = osd_g_force_pos;
+    msp_osd_config.osd_motor_diag_pos = osd_motor_diag_pos;
+    msp_osd_config.osd_log_status_pos = osd_log_status_pos;
+    msp_osd_config.osd_flip_arrow_pos = osd_flip_arrow_pos;
+    msp_osd_config.osd_link_quality_pos = osd_link_quality_pos;
+    msp_osd_config.osd_flight_dist_pos = osd_flight_dist_pos;
+    msp_osd_config.osd_stick_overlay_left_pos = osd_stick_overlay_left_pos;
+    msp_osd_config.osd_stick_overlay_right_pos = osd_stick_overlay_right_pos;
+    msp_osd_config.osd_display_name_pos = osd_display_name_pos;
+    msp_osd_config.osd_esc_rpm_freq_pos = osd_esc_rpm_freq_pos;
+    msp_osd_config.osd_rate_profile_name_pos = osd_rate_profile_name_pos;
+    msp_osd_config.osd_pid_profile_name_pos = osd_pid_profile_name_pos;
+    msp_osd_config.osd_profile_name_pos = osd_profile_name_pos;
+    msp_osd_config.osd_rssi_dbm_value_pos = osd_rssi_dbm_value_pos;
+    msp_osd_config.osd_rc_channels_pos = osd_rc_channels_pos;
+
+    msp.send(MSP_OSD_CONFIG, &msp_osd_config, sizeof(msp_osd_config));
+}
+
+void invert_pos(uint16_t *pos1, uint16_t *pos2)
+{
+    uint16_t tmp_pos = *pos1;
+    *pos1 = *pos2;
+    *pos2 = tmp_pos;
+}
